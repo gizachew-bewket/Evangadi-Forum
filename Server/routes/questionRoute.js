83df9b52c55719a682f4c14c4d019e8a1fccb2e4
Haile Dagne <haile.dange0@gmail.com>
2025-10-08 11:28:27 -0400

    uptodate server

--- Server/routes/questionRoute.js
+++ Server/routes/questionRoute.js
@@ -2,3 +2,16 @@ const express = require("express");
 const router = express.Router();
 // auth middleware
 const authmiddleware = require("../middleware/authmiddleware");
+
+// question controller
+const {
+  // Add your question controller functions here
+  // For example: get_questions, post_question, etc.
+} = require("../controllers/questioncontrollers");
+
+// Define your question routes here
+// For example:
+// router.get("/", authmiddleware, get_questions);
+// router.post("/", authmiddleware, post_question);
+
+module.exports = router;
