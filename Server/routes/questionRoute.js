0e2c775ea368fc9bbaee76c6aa2d0bec7836cc45
samsongybc <samsongybc@gmail.com>
2025-10-08 12:32:21 -0400

    questions server and client

--- Server/routes/questionRoute.js
+++ Server/routes/questionRoute.js
@@ -2,3 +2,21 @@ const express = require("express");
 const router = express.Router();
 // auth middleware
 const authmiddleware = require("../middleware/authmiddleware");
+
+// question controller
+const {
+  get_all_questions,
+  get_single_question,
+  post_question,
+} = require("../controllers/questioncontrollers");
+
+// get all questions
+router.get("/", authmiddleware, get_all_questions);
+
+// get single question
+router.get("/:questionid", authmiddleware, get_single_question);
+
+// post a question
+router.post("", authmiddleware, post_question);
+
+module.exports = router;
