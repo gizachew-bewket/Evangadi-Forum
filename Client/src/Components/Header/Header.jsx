f044fe595aeb8e716cacaf8acde0cfa1abfa5d22
Gizachew Bewket <gizachewbewket19@gmail.com>
2025-10-09 11:57:19 +0300

    update

--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { Appstate } from "../Pages/Appstate";
+import { Appstate } from "../../Pages/Appstate";
 import "./Header.css";
 
 const Header = () => {
+  const { user, setUser } = useContext(Appstate);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    localStorage.removeItem("userid");
+    setUser({});
+    navigate("/login");
+  };
+
   return (
-    <>
-      <header className="header">
-        <div className="header-left">
-          <div className="menu-icon">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+    <header className="header">
+      <div className="header-left">
+        <div className="menu-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <Link to="/home">
           <div className="logo">
             <span className="logo-orange">EV</span>
             <span className="logo-black">NGADI</span>
           </div>
-        </div>
+        </Link>
+      </div>
 
-        <nav className="nav">
-          <a href="#">Home</a>
-          <a href="#">How it works</a>
-          <button className="signin-btn">SIGN IN</button>
-        </nav>
-      </header>
-    </>
+      <nav className="nav">
+        <a href="/home">Home</a>
+        <Link to="/About">How it works</Link>
+
+        {!user?.username ? (
+          <button className="signin-btn" onClick={() => navigate("/login")}>
+            SIGN IN
+          </button>
+        ) : (
+          <button className="signin-btn" onClick={handleLogout}>
+            LOG OUT
+          </button>
+        )}
+      </nav>
+    </header>
   );
 };
 
